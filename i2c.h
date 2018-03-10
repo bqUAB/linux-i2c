2b7c796e8cfbcfbe480d226dfb3881d9d9f25f78
byque <byque2013@gmail.com>
2018-03-10 10:53:03 +0100

    Unfinished i2c basic demo
    I have to fix the arm compiler.

---
+++ i2c.h
@@ -0,0 +1,53 @@
+/* Simplified I2C class based on:
+http://docs.micropython.org/en/latest/wipy/library/machine.I2C.html
+*/
+#ifndef I2C_H
+#define I2C_H
+
+#include <stdio.h>          // Needed for printf, snprintf, perror
+#include <cstdint>  // Needed for unit uint8_t data type
+#include <fcntl.h>          // Needed for open()
+// #include <unistd.h>         // Needed for close()
+#include <stdlib.h>         // Needed for exit()
+// #include <sys/ioctl.h>      // Needed for ioctl
+// #include <linux/i2c-dev.h>  // Needed to use the I2C Linux driver (I2C_SLAVE)
+
+class I2cBus {
+  private:
+    int file_;
+    int list_[5];
+
+    
+
+  public:
+  /* --------------------------- General Methods --------------------------- */
+    int Scan();
+  /*Scan all I2C addresses between 0x08 and 0x77 inclusive and return a list
+  of those that respond. A device responds if it pulls the SDA line low after
+  its address (including a write bit) is sent on the bus.*/
+  /* -------------------------- Memory Operations --------------------------
+  Some I2C devices act as a memory device (or set of registers) that can be
+  read from and written to. In this case there are two addresses associated
+  with an I2C transaction: the slave address and the memory address. The
+  following methods are convenience functions to communicate with such
+  devices.*/
+
+    int ReadFromMem(int addr, int mem_addr, int n_bytes);
+  /* Read nbytes from the slave specified by addr starting from the memory
+  address specified by memaddr. The argument addrsize specifies the address
+  size in bits. Returns a bytes object with the data read.*/
+
+    void ReadFromMemInto(int addr, int memaddr, int* buff);
+  /* Read into buf from the slave specified by addr starting from the memory
+  address specified by memaddr. The number of bytes read is the length of buf.
+  The argument addrsize specifies the address size in bits (on ESP8266 this
+  argument is not recognised and the address size is always 8 bits).*/
+
+    void WriteToMem(int addr, int memaddr, int* buff);
+  /* Write buf to the slave specified by addr starting from the memory address
+  specified by memaddr. The argument addrsize specifies the address size in
+  bits (on ESP8266 this argument is not recognised and the address size is
+  always 8 bits).*/
+};
+
+#endif // I2C_H
---
+++ main.cc
@@ -0,0 +1,11 @@
+#include "i2c.h"
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+  //I2cBus i2c_0;
+  //i2c_0.file_ = 1;
+  //std::cout << i2f_0.file_;
+  printf("Test\n");
+  return 0;
+}
