f480a332afa24157bcd7ed03f3d0a87c40b4eb15
guillemma <guillem20121995@gmail.com>
2018-03-12 16:51:50 +0100

    I2c-read-MPU
    This  code give us the read of the 126 registers of the MPU.

--- main.cc
+++ main.cc
@@ -4,8 +4,16 @@
 
 int main() {
   I2cBus i2c_2(2);
+  
+  int valor[126]={0}; // valor del registro
   uint8_t value = i2c_2.ReadFromMem(0x68, 0x75) ; //MPU
   uint8_t value2 = i2c_2.ReadFromMem(0x1b, 0x03); //DLP
   printf("The value read is: 0x%X for the MPU and 0x%X for the DLP\n", value,value2);
+
+  for (int j=0;j<126;j++){
+  	valor[j]= i2c_2.ReadFromMem(0x68, j);
+  	printf("The value read is:0x%d\n", valor[j]);
+
+  }
   return 0;
 }
