dedfa08b685b19a96a8ce82b9110c9323d53727b
byque <byque2013@gmail.com>
2018-03-12 03:03:18 +0100

    Compiled write to memory

--- i2c.cc
+++ i2c.cc
@@ -16,13 +16,33 @@ I2cBus::I2cBus(int bus_n){
 }
 
 void I2cBus::Scan() {
+  /*Scan all I2C addresses between 0x08 and 0x77 inclusive and return a list
+  of those that respond. A device responds if it pulls the SDA line low after
+  its address (including a write bit) is sent on the bus.*/
+
   avail_addr = 0b1010011;  // 0x53
 }
 
-// void I2cBus::WriteToMem(uint8_t addr, uint8_t mem_addr){
-//   uint8_t w_buf[2];
-//
-//   // Write to defined register
-//   w_buf[0] = addr;
-//
-// }
+void I2cBus::WriteToMem(uint8_t addr, uint8_t mem_addr, uint8_t buff){
+  /* Write buf to the slave specified by addr starting from the memory address
+  specified by mem_addr.*/
+
+  if (ioctl(file_, I2C_SLAVE, addr) < 0) {
+        printf("Failed to acquire bus access and/or talk to slave.\n");
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        exit(1);
+    }
+
+  //uint8_t bSuccess = 0;
+  uint8_t w_buf[2];
+
+  /* Write to define register */
+  w_buf[0] = mem_addr;
+  w_buf[1] = buff;
+  if (write(file_, &w_buf, sizeof(w_buf)) == sizeof(w_buf)){
+    //bSuccess = 1;
+  } else {
+    /* ERROR HANDLING: i2c transaction failed */
+    //bSuccess = 0;
+  }
+}
--- i2c.h
+++ i2c.h
@@ -7,10 +7,10 @@ http://docs.micropython.org/en/latest/wipy/library/machine.I2C.html
 #include <stdio.h>          // Needed for printf, snprintf, perror
 #include <cstdint>  // Needed for unit uint8_t data type
 #include <fcntl.h>          // Needed for open()
-// #include <unistd.h>         // Needed for close()
+#include <unistd.h>         // Needed for write, close
 #include <stdlib.h>         // Needed for exit()
-// #include <sys/ioctl.h>      // Needed for ioctl
-// #include <linux/i2c-dev.h>  // Needed to use the I2C Linux driver (I2C_SLAVE)
+#include <sys/ioctl.h>      // Needed for ioctl
+#include <linux/i2c-dev.h>  // Needed to use the I2C Linux driver (I2C_SLAVE)
 
 class I2cBus {
   private:
@@ -24,15 +24,15 @@ class I2cBus {
 
   /* --------------------------- General Methods --------------------------- */
     void Scan();
-  /*Scan all I2C addresses between 0x08 and 0x77 inclusive and return a list
-  of those that respond. A device responds if it pulls the SDA line low after
-  its address (including a write bit) is sent on the bus.*/
+
   /* -------------------------- Memory Operations --------------------------
   Some I2C devices act as a memory device (or set of registers) that can be
   read from and written to. In this case there are two addresses associated
   with an I2C transaction: the slave address and the memory address. The
   following methods are convenience functions to communicate with such
   devices.*/
+
+    void WriteToMem(uint8_t addr, uint8_t memaddr, uint8_t buff);
 
     int ReadFromMem(uint8_t addr, uint8_t mem_addr, int n_bytes);
   /* Read nbytes from the slave specified by addr starting from the memory
@@ -45,11 +45,6 @@ class I2cBus {
   The argument addrsize specifies the address size in bits (on ESP8266 this
   argument is not recognised and the address size is always 8 bits).*/
 
-    void WriteToMem(uint8_t addr, uint8_t memaddr, int* buff);
-  /* Write buf to the slave specified by addr starting from the memory address
-  specified by memaddr. The argument addrsize specifies the address size in
-  bits (on ESP8266 this argument is not recognised and the address size is
-  always 8 bits).*/
 };
 
 #endif // I2C_H
