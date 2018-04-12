deced314fbfdf5ea03d6b744c4aa9526c0a1f749
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-12 17:38:06 +0200

    Improved the i2c-demo
    Added comments and a simple test to check the functionality of the program.

--- main.cc
+++ main.cc
@@ -1,3 +1,7 @@
+// I2C demo using a Terasic DE10 nano board which includes an ARM processor and
+// a Digital Accelerometer available at the I2C 0 bus. The board is running a
+// custom Linux distro that can be obtained from Terasic's web site.
+
 #include <stdio.h>
 #include "i2c.h"
 
@@ -5,7 +9,18 @@
 int main() {
   I2cBus i2c_0(0);
 
-  int value = i2c_0.ReadFromMem(0x53, 0x00);
+  // Digital Accelerometer
+  int address = 0x53;
+  int devid = 0;
+
+  int value = i2c_0.ReadFromMem(address, devid);
+  printf("The devid should be: 0xE5\n");
   printf("The HEX value read is: %#X\n", value);
+
+  if (value == 0xE5) {
+    printf("Test Passed\n" );
+  } else {
+    printf("Test not Passed\n");
+  }
   return 0;
 }
