9d10901f1939e5da11729079d4150760f36064a3
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-12 20:10:50 +0200

    Added read mpu gyro

---
+++ examples/read_mpu9250_gyro.cc
@@ -0,0 +1,40 @@
+// I2C example to read all MPU9250 gyro registers, the MPU is available at the
+// I2C 1 bus. The program is tested on ARM Linux.
+
+#include <stdio.h>
+#include "i2c.h"
+
+
+int main() {
+  I2cBus i2c_1(1);
+
+  int addr = 0x68;  // MPU9250 address
+  int gyro_xout_h = 0x43;  // starting MPU register address from which to read
+  uint8_t gyro[6] = {0};
+  int gyro_x = 0;
+  int gyro_y = 0;
+  int gyro_z = 0;
+
+  // Read 6 register values starting from gyro_xout_h
+  i2c_1.ReadFromMemInto(addr, gyro_xout_h, 6, gyro);
+  for (int i = 0; i < 6; i++) {
+    printf("gyro[%d] = %#X\n", i, gyro[i]);
+  }
+
+  gyro_x |= gyro[0] << 8;
+  // printf("Gyro x: %#X\n", gyro_x);
+  gyro_x |= gyro[1];
+  printf("Gyro x: %#X\n", gyro_x);
+
+  gyro_y |= gyro[2] << 8;
+  // printf("Gyro y: %#X\n", gyro_y);
+  gyro_y |= gyro[3];
+  printf("Gyro y: %#X\n", gyro_y);
+
+  gyro_z |= gyro[4] << 8;
+  // printf("Gyro y: %#X\n", gyro_z);
+  gyro_z |= gyro[5];
+  printf("Gyro y: %#X\n", gyro_z);
+
+  return 0;
+}
