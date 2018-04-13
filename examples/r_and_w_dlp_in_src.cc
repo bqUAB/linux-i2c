fd92a0cac16992f30158d4be8f05b11769df303c
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-13 16:47:37 +0200

    Added read and write dlp input source

---
+++ examples/r_and_w_dlp_in_src.cc
@@ -0,0 +1,33 @@
+// I2C example to write and read the DLPC2607 input source selection register.
+// The DLP is available at the I2C 1 bus. The program is tested on ARM Linux.
+
+#include <stdio.h>
+#include <cstdint>  // Needed for uint8_t
+#include "i2c.h"
+
+
+int main() {
+  I2cBus i2c_1(1);
+
+  int addr = 0x1B;  // DLPC2607 address
+  int reg = 0x0B;  // Input source selection
+  int value_in[4] = {0, 0, 0, 1};  // Parallel Interface
+  uint8_t value_out[4] = {0};
+  int value = 0;  // 32-bit value
+
+  // Write Command
+  i2c_1.WriteToMem(addr, reg, 4, value_in);
+
+  // Read Command
+  i2c_1.WriteToMem(addr, 0x15, 1, &reg);  // Part 1
+  i2c_1.ReadFromInto(addr, value_out);  // Part 2
+
+  for (int i = 0; i < int(sizeof(value_out)); i++) {
+    value |= value_out[i] << 8*(3 - i);
+    printf("value_out[%d] = %#X\n", i, value_out[i]);
+  }
+
+  printf("Value = %#X\n", value);
+
+  return 0;
+}
