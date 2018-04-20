6a817f8578208e2b426162f1a057c6f2d045f207
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-20 17:10:40 +0200

    Added read all mpu registers examples with found bug

--- examples/read_all_mpu9250_reg.cc
+++ examples/read_all_mpu9250_reg.cc
@@ -5,15 +5,35 @@
 #include "../i2c.h"
 
 int main() {
-  I2cBus i2c_1(1);
+  I2cBus i2c_bus(1);
 
-  int addr = 0x68;  // MPU9250 address
-  int values[128] = {0};  // MPU register values, not all of them are used
+  uint16_t addr = 0x68;  // MPU9250 address
+  uint8_t values[128] = {0};  // MPU register values, not all of them are used
+  uint8_t values_bug[128] = {0};
+  uint8_t values_no_bug[11] = {0};
 
-  for (int i = 0; i < 128; i++) {
-    values[i] = i2c_1.ReadFromMem(addr, i);
-    printf("The value read of the MPU 9250 register %#X ", i);
-    printf("is: %#X\n", values[i]);
+  printf("MPU9250 read all registers example\n");
+  printf("Register Address and Value\n");
+
+  for (uint i = 0; i < 128; i++) {
+    uint8_t data = 1;
+    if (i2c_bus.ReadFromMem(addr, i, &data)){
+      values[i] = data;
+    }
+    printf("%#X = %#X\n", i, values[i]);
+  }
+
+  // Bug
+  printf("Register Bug when using different methods\n");
+  i2c_bus.ReadFromMemInto(addr, 0, 128, values_bug);
+  for (uint i = 0; i < 128; i++) {
+    printf("values[%#X] = %#X,\t", i, values[i]);
+    printf("values_bug[%#X] = %#X\n", i, values_bug[i]);
+  }
+
+  i2c_bus.ReadFromMemInto(addr, 0x75, 11, values_no_bug);
+  for (uint i = 0; i < 11; i++) {
+    printf("values_no_bug[%#X] = %#X\n", i+0x75, values_no_bug[i]);
   }
 
   return 0;
